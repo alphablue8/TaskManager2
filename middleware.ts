193a197ca16ed4628d3b753009dcbb1422877616
Angga <maliky.anggoro@gmail.com>
2023-12-13 11:43:04 +0700

    test

--- middleware.ts
+++ middleware.ts
@@ -5,11 +5,4 @@ export default authMiddleware({ publicRoutes: ['/', '/api/webhook/clerk'], ignor
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
-import type { NextRequest } from 'next/server'
-import { NextResponse } from 'next/server'
- 
-export function middleware(request: NextRequest) {
-  const url = request.nextUrl.clone()
-  url.pathname = '/dest'
-  return NextResponse.rewrite(url)
-}
+
